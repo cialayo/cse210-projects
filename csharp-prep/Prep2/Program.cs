9887df232a0a0f165794f9b6fe5b671d0ca6b097
cialayo <cialayo@gmail.com>
2024-09-21 22:54:31 -0500

    Core Requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // program that determines the letter grade for a course
+        Console.Write("What is your grade percentage?");
+        string valueInText = Console.ReadLine();
+        int gradeInPercentage = int.Parse(valueInText);
+
+        if (gradeInPercentage >= 90)
+        {
+            Console.Write("You've got an A");
+        }
+        else if (gradeInPercentage < 90 && gradeInPercentage >= 80)
+        {
+            Console.Write("You've got a B");
+        }
+        else if (gradeInPercentage < 80 && gradeInPercentage >= 70)
+        {
+            Console.Write("You've got a C");
+        }
+        else if (gradeInPercentage < 70 && gradeInPercentage >= 60)
+        {
+            Console.Write("You've got a D");
+        }
+        else
+        {
+            Console.Write("You've got an F");
+
+        }
     }
 }
