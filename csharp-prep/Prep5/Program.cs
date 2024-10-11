4c70030342557c5f1b6fae50912658de0bd4627e
cialayo <cialayo@gmail.com>
2024-10-10 21:16:50 -0500

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+
+        string userName = DisplayUserName();
+        int userNumber = DisplayUserNumber();
+        int squaredNumber = SquareOfNumber(userNumber);
+
+        SquaredNumberMessage(userName,squaredNumber);
+    }
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string DisplayUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int DisplayUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int SquareOfNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+    static void SquaredNumberMessage(string name, int square )
+    {
+        string message = $"{name}, the square of your number is {square}";
+        Console.WriteLine(message);
     }
 }
