c16372d9987df27eff5214d158522293edcd4165
cialayo <cialayo@gmail.com>
2024-10-22 23:07:24 -0500

    Learning activity

---
+++ prepare/Learning05/Assignment.cs
@@ -0,0 +1,15 @@
+public class Assignment
+{
+    protected string _studentName;
+    protected string _topic;
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetSummary()
+    {
+        string summary = $"{_studentName} - {_topic}";
+        return summary;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment example = new Assignment("Samuel Bennnet", "Multiplication");
+        string summary = example.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine("");
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        string info = math.GetHomeworkList();
+        Console.WriteLine(info);
+        Console.WriteLine("");
+        WritingAssignment write = new WritingAssignment("Mary Waters", "European History", "The Cuases of Worl War II");
+        string writingInformation = write.GetWritingInformation();
+        Console.WriteLine(writingInformation);
+
     }
 }
